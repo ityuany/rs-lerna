88e44c705ee20e5d140e3c626e11648bedb086e2
ityuany <519495771@qq.com>
2024-02-18 00:41:42 +0800

    iterm style

--- packages/fetch-node-remote/src/main.rs
+++ packages/fetch-node-remote/src/main.rs
@@ -1,11 +1,12 @@
-use std::collections::hash_map::Values;
-use chrono::{DateTime, Utc};
+use chrono::{NaiveDate, Utc};
 use colored::*;
 use reqwest;
-use semver::{Version,VersionReq};
+use semver::{Version, VersionReq};
 use serde::Deserialize;
-use serde_json::{Value, Map, json, to_string_pretty};
+use serde_json::{json, to_string_pretty, Map, Value};
+use std::collections::hash_map::Values;
 use std::collections::HashMap;
+use std::fmt::format;
 
 #[derive(Debug, Deserialize)]
 pub struct NodeMeta {
@@ -21,40 +22,47 @@ pub struct NodeMeta {
     pub lts: Option<Value>,
     pub security: Option<Value>,
 }
+
 #[derive(Debug)]
-pub struct NodeScheduleH {
+pub enum Lts {
+    Str(String),
+    Bool(bool),
+}
+
+#[derive(Debug)]
+pub struct NodeMeta1 {
+    version: String,
+    date: String,
+    lts: Lts,
+}
+
+#[derive(Debug)]
+pub struct NodeSchedule {
     pub start: String,
     pub end: String,
     pub version: String,
 }
 
-fn fetch_node_schedule() -> Vec<NodeScheduleH>{
+fn fetch_node_schedule() -> Vec<NodeSchedule> {
     let resp = reqwest::blocking::get(
         "https://raw.githubusercontent.com/nodejs/Release/main/schedule.json",
     )
-        .expect("fetch nodejs.org/dist/index.json failed.");
-    let res: Value =
-        resp.json::<Value>().expect("Error");
+    .expect("fetch nodejs.org/dist/index.json failed.");
+    let res: Value = resp.json::<Value>().expect("Error");
 
-
-    let mut v: Vec<NodeScheduleH> = Vec::new();
+    let mut v: Vec<NodeSchedule> = Vec::new();
 
     if let Some(json) = res.as_object() {
         json.keys().for_each(|key| {
             if let Some(item) = json.get(key) {
-
                 let version = &key.as_str()[1..];
 
-                v.push(
-                    NodeScheduleH {
-                        start: item.get("start").unwrap().to_string(),
-                        end: item.get("end").unwrap().to_string(),
-                        version: version.to_string(),
-                    }
-                );
-
+                v.push(NodeSchedule {
+                    start: item.get("start").unwrap().to_string(),
+                    end: item.get("end").unwrap().to_string(),
+                    version: version.to_string(),
+                });
             }
-
         });
     }
 
@@ -63,48 +71,99 @@ fn fetch_node_schedule() -> Vec<NodeScheduleH>{
     v
 }
 
-fn fetch_node_versions() -> Vec<(Version, String)> {
+fn fetch_node_versions() -> Vec<NodeMeta1> {
     let resp = reqwest::blocking::get("https://nodejs.org/dist/index.json")
         .expect("fetch nodejs.org/dist/index.json failed.");
 
     let json: Vec<NodeMeta> = resp.json().expect("Error");
 
+    let mut vv: Vec<NodeMeta1> = Vec::new();
+
     let mut json: Vec<_> = json
         .into_iter()
-        .filter_map(|node| {
-            let version = Version::parse(&node.version[1..]).unwrap();
-            match &node.lts {
-                Some(Value::String(lts)) => Some((
-                    version,
-                    format!(
-                        "{:<10} {:<12} {}",
-                        node.version,
-                        node.date.bright_black(),
-                        lts.bright_green()
-                    ),
-                )),
-                Some(Value::Bool(lts)) if *lts == false => Some((
-                    version,
-                    format!("{:<10} {:<12}", node.version, node.date.bright_black()),
-                )),
-                _ => None,
-            }
+        .map(|node| NodeMeta1 {
+            version: node.version[1..].to_string(),
+            date: node.date,
+            lts: match &node.lts {
+                Some(Value::String(lts)) => Lts::Str(lts.to_string()),
+                Some(Value::Bool(lts)) => Lts::Bool(*lts),
+                _ => Lts::Bool(false),
+            },
         })
         .collect();
 
-    json.sort_by(|a, b| a.0.cmp(&b.0));
+    json.sort_by(|a, b| {
+        let av = Version::parse(&a.version).expect("Error");
+        let bv = Version::parse(&b.version).expect("Error");
+        av.cmp(&bv)
+    });
 
     json
 }
 
 fn main() {
-    let json = fetch_node_schedule();
-    json.iter().for_each(|node| {
-        println!("{:?}", node);
-    });
+    let sss = String::from("2014-07-31");
 
-    // let json = fetch_node_versions();
-    // for (_, line) in json {
-    //     println!("{}", line);
-    // }
+    let s = NaiveDate::parse_from_str(&sss, "%Y-%m-%d").expect("parse end date error");
+
+    println!("{:?}", s);
+
+    let s = VersionReq::parse("9").unwrap();
+
+    let a = Version::parse("0.9.0").unwrap();
+
+    println!("{}", s.matches(&a));
+
+    let json = fetch_node_schedule();
+
+    let find_schedule = |item: &NodeMeta1| {
+        json.iter().find(|schedule| {
+            VersionReq::parse(&schedule.version)
+                .unwrap()
+                .matches(&Version::parse(&item.version).unwrap())
+        })
+    };
+
+    let json = fetch_node_versions();
+    for item in json {
+        let res = find_schedule(&item);
+
+        let is_active = if let Some(schedule) = res {
+            // println!("{} , {:?}", schedule.end, res);
+            let end =
+                NaiveDate::parse_from_str(&schedule.end.trim_matches('"'), "%Y-%m-%d").unwrap();
+            end > Utc::now().date_naive()
+        } else {
+            false
+        };
+
+        let lts_str = match item.lts {
+            Lts::Str(s) => {
+                if is_active {
+                    s.green().to_string()
+                } else {
+                    s
+                }
+            }
+            _ => "".to_string(),
+        };
+
+        // let v = if is_active {
+        //     item.version
+        // } else {
+        //     item.version.strikethrough().bright_black().to_string()
+        // };
+
+        let min_width = if is_active { 10 } else { 0 };
+
+        let line = format!("{:<min_width$} {}", item.version, lts_str);
+
+        if is_active {
+            println!("{}", line);
+        } else {
+            println!("{}", line.strikethrough().bright_black().to_string());
+        }
+
+        // println!("{}", line);
+    }
 }
